57a65d2ea971228256ab665e5deb11e09c0e40bb
João Ferreira <jofelippe@hotmail.com>
2018-08-07 16:05:30 -0300

    Initial commit

---
+++ src/main/java/br/com/nexfe/siesma/SiesmaBackendApplication.java
@@ -0,0 +1,12 @@
+package br.com.nexfe.siesma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiesmaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiesmaBackendApplication.class, args);
+	}
+}
